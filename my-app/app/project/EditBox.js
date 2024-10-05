36b027ff43f49ccc45bd15120294d47e6df9ca63
jonss0777 <jsuconota1@gmail.com>
2024-10-05 19:52:08 -0400

    creating boxes for the project page

---
+++ my-app/app/project/EditBox.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { Box } from '@mui/material';
+
+const EditBox = ({ children,  primaryColor, primaryDark, pwidth, pheight }) => {
+    // Define the theme using the passed parameters
+    const theme = {
+        palette: {
+            primary: {
+                main: primaryColor,
+                dark: primaryDark,
+            },
+        },
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box
+                sx={{
+                    width: pwidth,
+                    height: pheight,
+                    borderRadius: 0,
+                    bgcolor: 'primary.main',
+                    '&:hover': {
+                        bgcolor: 'primary.dark',
+                    },
+                }}
+            >
+              {children}
+
+            </Box>
+        </ThemeProvider>
+    );
+};
+
+
+export default EditBox;
--- my-app/app/project/page.js
+++ my-app/app/project/page.js
@@ -1,10 +1,22 @@
 'use client'
+import EditBox from "./EditBox";
+import SimpleContainer from "./SimpleContainer";
 
 export default function Home() {
-    return (
-      <div >
-        <p>Project route</p>
-        
-      </div>
-    );
-  }
+  return (
+    <div >
+      <SimpleContainer>
+        <EditBox primaryColor="red" primaryDark="white" pwidth={2000} pheight={1000}>
+
+
+        </EditBox>
+
+      </SimpleContainer>
+
+
+
+
+
+    </div>
+  );
+}
