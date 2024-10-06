eb07e2ee1d9bf275d1cf01beacde596313af4b9a
jonss0777 <jsuconota1@gmail.com>
2024-10-05 22:38:46 -0400

    editing page2

--- my-app/app/project/page.js
+++ my-app/app/project/page.js
@@ -1,43 +1,32 @@
 'use client'
-import { Edit } from "@mui/icons-material";
-import EditBox from "./EditBox";
-import SimpleContainer from "./SimpleContainer";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
 
-
-import { Grid2 } from '@mui/material';
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 export default function Home() {
-
   return (
-    <>
-      <Grid2 columns={2} columnSpacing={0} container={true} wrap="nowrap">
-
-        <EditBox primaryColor="white" pwidth={750} pheight={1000}>
-          {/* Image */}
-          <EditBox primaryColor="brown" ryColor="white" pwidth={750} pheight={50}>
-            <p>Image box</p>
-          </EditBox>
-          {/* Text area */}
-          <EditBox primaryColor="brown" ryColor="white" pwidth={750} pheight={50}>
-            <p>Text Area</p>
-          </EditBox>
-          {/* Project pic area */}
-          <EditBox primaryColor="brown" ryColor="white" pwidth={750} pheight={50}>
-            <p>Project pic </p>
-          </EditBox>
-
-          {/* Discussion Board */}
-          <EditBox primaryColor="brown" ryColor="white" pwidth={750} pheight={50}>
-            <p> Discussion Board </p>
-          </EditBox>
-
-      </EditBox>
-
-
-      <EditBox primaryColor="skyblue" pwidth={250} pheight={1000}></EditBox>
-    </Grid2 >
-
-
-    </>
+    <Box width={2000} height={1000} sx={{ flexGrow: 1 }}>
+      <Grid container spacing={0} direction="row">
+        <Grid size={{ xs: 6, md: 8 }}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+        <Grid size={{ xs: 6, md: 4 }}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        
+      </Grid>
+    </Box>
   );
 }
