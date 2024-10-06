6331e9b7b714aeab04a26a98f6eb767e0b48d318
jonss0777 <jsuconota1@gmail.com>
2024-10-06 04:26:35 -0400

    edit project/page.js

--- my-app/app/project/page.js
+++ my-app/app/project/page.js
@@ -27,31 +27,31 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const itemData = [
   {
-    img: '',
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
   },
   {
-    img: '',
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     title: 'Burger',
   },
   {
-    img: '',
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
     title: 'Camera',
   },
   {
-    img: '',
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
     title: 'Coffee',
   },
   {
-    img: '',
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
     title: 'Hats',
   },
   {
-    img: '',
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
   },
   {
-    img: '',
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
     title: 'Basketball',
   },
   {
